770139044562b31bb8f3b1790c974d933dd327f0
maggievu <maggievu91@gmail.com>
2019-03-06 21:30:07 -0800

    Updates

---
+++ precache-manifest.8420ae16ebbf83b126a368025f884f6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6332712eb284ea976e12",
+    "url": "/oak-and-fort-coding-exercise/static/js/runtime~main.dd94fa4b.js"
+  },
+  {
+    "revision": "a375b9247e703772efe2",
+    "url": "/oak-and-fort-coding-exercise/static/js/main.538a66da.chunk.js"
+  },
+  {
+    "revision": "190431e05092d4d3ccfd",
+    "url": "/oak-and-fort-coding-exercise/static/js/2.233c4403.chunk.js"
+  },
+  {
+    "revision": "a375b9247e703772efe2",
+    "url": "/oak-and-fort-coding-exercise/static/css/main.16788f5b.chunk.css"
+  },
+  {
+    "revision": "e7ad27d552153e60637ab425ec255634",
+    "url": "/oak-and-fort-coding-exercise/index.html"
+  }
+];
